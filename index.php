52f1be1873f83386870521b51d7dbb2f6827bdef
Jesusgarza25 <pronto_reto@hotmail.com>
2018-11-20 14:10:31 -0500

    movimiento

--- index.php
+++ index.php
@@ -42,7 +42,7 @@
           <div class="android-navigation-container">
             <nav class="android-navigation mdl-navigation">
               <a class="mdl-navigation__link mdl-typography--text-uppercase" href="index.html">Home</a>
-              <a class="mdl-navigation__link mdl-typography--text-uppercase" href="">Entregas</a>
+              <a class="mdl-navigation__link mdl-typography--text-uppercase" href="">Catalogo</a>
               <a class="mdl-navigation__link mdl-typography--text-uppercase" href="uploading.html">Uploads</a>
               <a class="mdl-navigation__link mdl-typography--text-uppercase" href="informacion.html">Informacion</a>
               <a class="mdl-navigation__link mdl-typography--text-uppercase" href="contactos.html">Carrito</a>
@@ -81,7 +81,91 @@ else{
     $sesion="Iniciar";
 }
               ?>
+
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+* {box-sizing: border-box;}
+body {font-family: Verdana, sans-serif;}
+.mySlides {
+  
+  width: 800px;
+  display: none;}
+
+
+
+
+/* Slideshow container */
+.slideshow-container {
+  max-width: 1000px;
+  position: relative;
+  width: -800%;
+  margin: auto;
+}
+
+/* Caption text */
+.text {
+  color: black;
+  font-size: 15px;
+  padding: 8px 12px;
+  position: absolute;
+  bottom: 8px;
+  width: 100%;
+  text-align: center;
+}
+
+/* Number text (1/3 etc) */
+.numbertext {
+  color: #f2f2f2;
+  font-size: 12px;
+  padding: 8px 12px;
+  position: absolute;
+  top: 0;
+}
+
+
+/* The dots/bullets/indicators */
+.dot {
+  height: 15px;
+  width: 15px;
+  margin: 0 2px;
+  background-color: #bbb;
+  border-radius: 50%;
+  display: inline-block;
+  transition: background-color 0.6s ease;
+}
+
+.active {
+  background-color: #717171;
+}
+
+}
+
+/* Fading animation */
+.fade {
+  -webkit-animation-name: fade;
+  -webkit-animation-duration: 1.5s;
+  animation-name: fade;
+  animation-duration: 1.5s;
+}
+
+@-webkit-keyframes fade {
+  from {opacity: .4} 
+  to {opacity: 1}
+}
+
+@keyframes fade {
+  from {opacity: .4} 
+  to {opacity: 1}
+}
+
+/* On smaller screens, decrease text size */
+@media only screen and (max-width: 300px) {
+  .text {font-size: 11px}
+}
+</style>
+
           <span class="android-mobile-title mdl-layout-title">
+
             <img class="android-logo-image" src="images/ekoseats.png">
           </span>
         </div>
@@ -152,41 +236,49 @@ else{
         <div class="android-screen-section mdl-typography--text-center">
           <a name="screens"></a>
           <div class="mdl-typography--display-1-color-contrast">Conoce toda nuestra linea de productos</div>
+          <br>
 <?php
-date_default_timezone_set("America/New_York");
-$dia = date("l");
+
             echo "<div class='android-screens'>";
-            echo"<div class='android-wear android-screen'>";
-             echo "<a class='android-image-link' href=''>
-             <img class='android-screen-image' src='images/wear.jpg'>";
-                
-              echo"</a>";
-            echo"<a class='android-link mdl-typography--font-regular mdl-typography--text-uppercase' href=''>$dia</a>";
+            echo"<div class='android-wear android-screen'>";                
+          
             echo"</div>";
             
+
             echo"<div class='android-phone android-screen'>
-              <a class='android-image-link' href=''>
-                <img class='android-screen-image' src='images/silla.jpg'>
-              </a>
-              <a class='android-link mdl-typography--font-regular mdl-typography--text-uppercase' href=''>$dia</a>
-            </div>
-            <div class='android-tablet android-screen'>
-              <a class='android-image-link' href=''>
-                <img class='android-screen-image' src='images/si2.jpg'>
-              </a>
-              <a class='android-link mdl-typography--font-regular mdl-typography--text-uppercase' href=''>$dia</a>
-            </div>
-            <div class='android-tv android-screen'>
-              <a class='android-image-link' href=''>
-                <img class='android-screen-image' src='images/mesa.jpg'>
-              </a>
-              <a class='android-link mdl-typography--font-regular mdl-typography--text-uppercase' href=''>$dia</a>
-            </div>
-            <div class='android-auto android-screen'>
-              <a class='android-image-link' href=''>
-                <img class='android-screen-image' src='images/mueble5.jpg'>
-              </a>
-              <a class='android-link mdl-typography--font-regular mdl-typography--text-uppercase mdl-typography--text-left' href=''>$dia</a>
+              
+            <div class='slideshow-container'>
+
+<div class='mySlides fade'>
+  <div class='numbertext'>1 / 3</div>
+  <img src='images/1.jpg' style='width:100%''>
+  <div class='text'>Sala </div>
+</div>
+
+<div class='mySlides fade'>
+  <div class='numbertext'>2 / 3</div>
+  <img src='images/sala2.jpg' style='width:100%''>
+  <div class='text'>Sala</div>
+</div>
+
+<div class='mySlides fade'>
+  <div class='numbertext'>3 / 3</div>
+  <img src='images/salas3.jpg' style='width:100%''>
+  <div class='text'>Sala</div>
+</div>
+
+</div>
+<br>
+
+<div style='text-align:center'>
+  <span class='dot'></span> 
+  <span class='dot'></span> 
+  <span class='dot'></span> 
+</div>
+
+
+
+              <a class='android-link mdl-typography--font-regular mdl-typography--text-uppercase mdl-typography--text-left' href=''></a>
             </div>
           </div>
         </div>
@@ -290,8 +382,7 @@ $dia = date("l");
               </div>
             </div>
           </div>
-        </div>
-        ";
+        </div>";
                 ?>
 
 
@@ -345,11 +436,30 @@ $dia = date("l");
         </footer>
       </div>
     </div>
+
+    <script>
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    for (i = 0; i < slides.length; i++) {
+       slides[i].style.display = "none";  
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}    
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";  
+    dots[slideIndex-1].className += " active";
+    setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
+</script>
  
     <script src="js/material.min.js"></script>
-
-
-
 
   </body>
 </html>
