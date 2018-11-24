142b6eb43dd69ef6bbba81a24507503905227d3e
Jesusgarza25 <pronto_reto@hotmail.com>
2018-11-24 16:34:02 -0500

    "redireccinamiento"

--- regis.php
+++ regis.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="description" content="Ejemplo de Material Design">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Registrarse</title>
+    <title>EkoSeatS</title>
     <!-- Prueba en GitHub desde StarPlatinumUwU -->
     <!-- Prueba en GitHub desde AlienatedFac -->
      <!-- Prueba en GitHub desde Yisus -->
@@ -13,22 +13,26 @@
  
     <!-- Estilos -->
  
-    <link href="https://fonts.googleapis.com/css?family=Roboto:regular,bold,italic,thin,light,bolditalic,black,medium&amp;lang=es" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-    <link rel="stylesheet" href="css/material.min.css">
-    <link rel="stylesheet" href="css/styles.css">
-    <link rel="stylesheet" href="css/registro.css">
-    <link rel="shortcut icon" href="icono.ico" />
+    <link href="https://fonts.googleapis.com/css?family=Roboto:regular,bold,italic,thin,light,bolditalic,black,medium&amp;lang=es" rel="stylesheet"media="screen">
+
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"media="screen">
+    <link rel="stylesheet" href="css/material.min.css"media="screen">
+    <link rel="stylesheet" href="css/styles.css" media="screen">
+    <link rel="shortcut icon" href="icono.ico" media="screen">
+      <link rel="stylesheet" href="css/registro.css" media="screen">
+     
    <!-- Mi Head we --> 
   </head>
+
   <body>
     <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
 
       <div class="android-header mdl-layout__header mdl-layout__header--waterfall">
         <div class="mdl-layout__header-row">
           <span class="android-title mdl-layout-title">
-            <img class="android-logo-image" src="images/ekoseats.png">
+            <img class="android-logo-image" src="images/ekoseats.png" >
           </span>
+
           <!-- Agregamos un espacio entre el logo de y el menú -->
           <div class="android-header-spacer mdl-layout-spacer"></div>
           <div class="android-search-box mdl-textfield mdl-js-textfield mdl-textfield--expandable mdl-textfield--floating-label mdl-textfield--align-right mdl-textfield--full-width">
@@ -40,52 +44,56 @@
             </div>
           </div>
           <!-- Menú -->
-            <?php
-            echo" <div class='android-navigation-container'>
-            <nav class='android-navigation mdl-navigation'>
-               <a class='mdl-navigation__link mdl-typography--text-uppercase' href='index.php'>Home</a>
-              <a class='mdl-navigation__link mdl-typography--text-uppercase' href='catalogo.php'>Catalogo</a>
-              <a class='mdl-navigation__link mdl-typography--text-uppercase' href='contact.html'>Contactanos</a>
-              <a class='mdl-navigation__link mdl-typography--text-uppercase' href='informacion.html'>Nosotros</a>
-              <br>
-              <br>
-              <a class='mdl-navigation__link mdl-typography--text-uppercase' href=''>Registro</a>
-              <a class='mdl-navigation__link mdl-typography--text-uppercase' href='Log-in.html'>Inicio de Sesion</a>
-
-            </nav>
-          </div>
-          <button class='android-more-button mdl-button mdl-js-button mdl-button--icon mdl-js-ripple-effect' id='more-button'>
-            <i class='material-icons'>more_vert</i>
-            </button>
-    <ul class='mdl-menu mdl-js-menu mdl-menu--bottom-right mdl-js-ripple-effect' for='more-button'>
-            <li class='mdl-menu__item' href='Log-in.html'> <a  href='Log-in.html'> Sesion </a></li>";
- 
+        <?php
+error_reporting(0);
 $user=$_POST['user'];
-$password=$_POST['password'];
-            $sesion="";
+$password=$_POST['password']; 
+if(empty($user and $password))
+{
+   echo ""; 
+}
 session_start();
-
 $conexion = mysqli_connect("localhost", "root", "", "EkoBDD");
-$consulta =$conexion->query("SELECT * from ACCESO WHERE Nombre='$user' AND Password='$password'");
-           
-    
-
+$consulta =$conexion->query("SELECT * from ACCESO WHERE Nombre='$user' AND Password='$password'");       
 if($resultado = mysqli_fetch_array($consulta)){
-	$login="Cerrar Sesion";
-    $_SESSION['u']=$user;
-    $sesion="Cerrar";
-echo" 
-            <li class='mdl-menu__item'>$user</li>
-            <li class='mdl-menu__item'>Administrador</li>
-            <li class='mdl-menu__item' href='index.php'>Cerrar Sesion</li>
-          </ul>";
+  $_SESSION['u']=$user;
+            echo "<script type=\"text/javascript\">alert(\"Bienvenido $user\");</script>";
+    $registro="index.php";
+    $regis="Logout";
+    $sesion="index.php";
+    $ses=$user;
     
 }
-else{
-    $sesion="Iniciar";
-}
-              ?>
+            else{
+                $registro="regis.php";
+                $regis="Registrate";
+                $sesion="Log-in.html";
+    $ses="Login";
+                
+            }
+            
+ echo"
+          <div class='android-navigation-container'  >
+            <nav class='android-navigation mdl-navigatio' >
+
+              <a class='mdl-navigation__link mdl-typography--text-uppercase' href='index.php' >Home</a>
+              <a class='mdl-navigation__link mdl-typography--text-uppercase' href='catalogo.php'>Catalogo</a>
+              <a class='mdl-navigation__link mdl-typography--text-uppercase' href='contact.php'>Contactanos</a>
+              <a class='mdl-navigation__link mdl-typography--text-uppercase' href='informacion.php'>Nosotros</a>
+            <a class='mdl-navigation__link mdl-typography--text-uppercase' id='open-modal-login' href='$registro'>$regis</a>
+             <a class='mdl-navigation__link mdl-typography--text-uppercase' id='open-modal-login' href='$sesion'>$ses</a>
+            </nav>
+            
+    <ul class='mdl-menu mdl-js-menu mdl-menu--bottom-right mdl-js-ripple-effect' for='more-button'>
+                </ul>
+
+           
+          </div>
+";
+
+ ?>
           <span class="android-mobile-title mdl-layout-title">
+
             <img class="android-logo-image" src="images/ekoseats.png">
           </span>
         </div>
@@ -138,8 +146,10 @@ else{
 </form>
         </nav>
       </div>
+
+      <div class="android-content mdl-layout__content">
 <div class="android-content mdl-layout__content">
-  <form action="refistrado.php" method="POST">
+  <form action="" method="POST">
   <div class="container">
     <h1>Registrarse!</h1>
     <p>Por favor llena los datos para registrarte.</p>
@@ -173,7 +183,38 @@ else{
 </form>
     
 </div>
+ <footer class="android-footer mdl-mega-footer">
+          <div class="mdl-mega-footer--top-section">
+            
+            <div class="mdl-mega-footer--right-section">
+             
+            </div>
+          </div>
+
+          <div class="mdl-mega-footer--middle-section">
+            <p class="mdl-typography--font-light">Ekoseats © 2018 Mexico</p>
+            <p class="mdl-typography--font-light">Algunas características pueden no estar disponibles en todas las áreas</p>
+          </div>
+
+          <div class="mdl-mega-footer--bottom-section">
+           
+         
+            
+            <a class="android-link mdl-typography--font-light" href="">Blog></a>
+            <a class="android-link mdl-typography--font-light" href="">Política de Privacidad</a>
+          </div>
+
+        </footer>
+      </div>
+    </div>
+
+ 
+    <script src="js/material.min.js"></script>
 </div>
+
+
+       
+
 </body>
 
 </html>
