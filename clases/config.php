2ca18446f70006fcc7976347e09c25abe771515f
Fernando Araujo <substratum21@gmail.com>
2018-11-28 17:47:26 -0500

    <<<<<<<<<<<<<<<<<final

--- clases/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-	session_start();
-	define('DB_HOST', 'localhost');
-	define('DB_USER', 'root');
-	define('DB_PASS', 'avalos12');
-	define('DB_NAME', 'tutoriales_carrito');
-	define('DB_CHARSET', 'utf8');
-?>
