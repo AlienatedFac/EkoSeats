41c0fba73d092d7212bc43f2db1b3786715721d9
Jesusgarza25 <pronto_reto@hotmail.com>
2018-11-28 04:03:45 -0500

    sdsdsd

--- Producto.php
+++ Producto.php
@@ -177,10 +177,10 @@ $var1=$resultado['Tipo'];
         </nav>
       </div>
 
-        <div class="android-content mdl-layout__content">
+      
 <style>
 body, html {
-    height: 200%;
+    height: 100%;
     font-family: Arial, Helvetica, sans-serif;
 }
 
@@ -193,7 +193,7 @@ body, html {
     background-image: url("images/mueblec.jpg");
 
     min-height: 550px;
-    width: 900px;
+    width: 800px;
 left: 50px;
     /* Center and scale the image nicely */
     background-position: center;
@@ -206,7 +206,7 @@ left: 50px;
 .container {
 
     position: relative;
-left:  205px;
+left:  805px;
     margin: 20px;
     max-width:650px;
     height: 600px;
@@ -293,20 +293,7 @@ if($resultado = mysqli_fetch_array($consulta)){
 
 ";
 ?>
-<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
-<input type="hidden" name="cmd" value="_s-xclick">
-<input type="hidden" name="hosted_button_id" value="9KCYJFB89F4NQ">
-<table>
-<tr><td><input type="hidden" name="on0" value="Pago">Pago</td></tr><tr><td>
-    <select name="os0">
-    <option value="Opción 1">Opción 1 $200.00 MXN</option>
-  
-</select> </td></tr>
-</table>
-<input type="hidden" name="currency_code" value="MXN">
-<input type="image" src="https://www.paypalobjects.com/es_XC/MX/i/btn/btn_buynowCC_LG.gif" border="0" name="submit" alt="PayPal, la forma más segura y rápida de pagar en línea.">
-<img alt="" border="0" src="https://www.paypalobjects.com/es_XC/i/scr/pixel.gif" width="1" height="1">
-</form>
+
 
 
    <footer class="android-footer mdl-mega-footer">
@@ -331,7 +318,8 @@ if($resultado = mysqli_fetch_array($consulta)){
           </div>
 
         </footer>
-      </div>
+      
+
 
     <script src="js/material.min.js"></script>
 
